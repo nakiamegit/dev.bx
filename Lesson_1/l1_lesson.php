ad54ff23f265ea046c52ca41b2a3d69ef64533d2
Nak1ame <nak1ame@mail.ru>
2020-11-16 18:46:31 +0200

    Задание 1

--- Lesson_1/l1_lesson.php
+++ Lesson_1/l1_lesson.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-require_once '../Lesson_1/function.php';
+require_once '../Lesson_1/l1_function.php';
 
 echo '========= Задание 1. Калькулятор =========' . PHP_EOL;
 echo 'Для завершения работы оставьте поле пустым и нажмите Enter.' . PHP_EOL;
